55587e81dfe451686abd71210de5ebce3d2dff36
Michael Eder <spam@michael-eder.net>
2019-08-14 21:54:12 +0200

    Remove more unused code

--- cmd/scan.go
+++ cmd/scan.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"github.com/nray-scanner/nray/utils"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
@@ -15,14 +14,9 @@ var scanCmd = &cobra.Command{
 creating a configuration and attaching scanner nodes, the simple scan
 is what you are looking for. Get the work done nmap-style like you are used to.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		ports, err := utils.ParsePorts(rawPorts)
-		utils.CheckError(err, false)
-		targets, err := utils.ParseTargets(args)
-		utils.CheckError(err, false)
 
-		log.Infof("Scan called. Task is to scan %s on ports %v", targets, ports)
-		log.Info("Well actually somebody needs to implement this first^^")
-		log.Info("Til then, use the server client stuff and have a nice day")
+		log.Info("Well actually somebody needs to implement this first...")
+		log.Info("Until then, please use the server/client functionality and have a nice day")
 
 	},
 }
