02964857bbe80a85292bc49f1e80d7463f4df208
Michael Eder <spam@michael-eder.net>
2019-09-19 23:09:13 +0200

    more tests for default options

--- utils/defaultConfig_test.go
+++ utils/defaultConfig_test.go
@@ -8,6 +8,259 @@ import (
 	"github.com/spf13/viper"
 )
 
+func TestApplyDefaultConfig(t *testing.T) {
+	var result *viper.Viper
+
+	// Test passing nil to the function
+	result = utils.ApplyDefaultConfig(nil)
+	if !result.IsSet("debug") || result.GetBool("debug") != false {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+	if !result.IsSet("listen") {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+	if !result.IsSet("host") || result.GetString("host") != "127.0.0.1" {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+	if !result.IsSet("TLS.enabled") || result.GetBool("TLS.enabled") != false {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+	if !result.IsSet("TLS.CA") || result.GetString("TLS.CA") != "" {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+	if !result.IsSet("TLS.cert") || result.GetString("TLS.cert") != "" {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+	if !result.IsSet("TLS.key") || result.GetString("TLS.key") != "" {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+	if !result.IsSet("TLS.forceClientAuth") || result.GetBool("TLS.forceClientAuth") != false {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+	if !result.IsSet("pools") || result.GetUint("pools") != 1 {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+	if !result.IsSet("considerClientPoolPreference") || result.GetBool("considerClientPoolPreference") != true {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+	if !result.IsSet("internal.nodeExpiryTime") || result.GetUint("internal.nodeExpiryTime") != 30 {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+	if !result.IsSet("internal.nodeExpiryCheckInterval") || result.GetUint("internal.nodeExpiryCheckInterval") != 10 {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+	if !result.IsSet("targetgenerator.bufferSize") || result.GetUint("targetgenerator.bufferSize") != 5 {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+
+	// Test passing an empty viper to the function
+	emptyViper := viper.New()
+	result = utils.ApplyDefaultConfig(emptyViper)
+	if !result.IsSet("debug") || result.GetBool("debug") != false {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+	if !result.IsSet("listen") {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+	if !result.IsSet("host") || result.GetString("host") != "127.0.0.1" {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+	if !result.IsSet("TLS.enabled") || result.GetBool("TLS.enabled") != false {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+	if !result.IsSet("TLS.CA") || result.GetString("TLS.CA") != "" {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+	if !result.IsSet("TLS.cert") || result.GetString("TLS.cert") != "" {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+	if !result.IsSet("TLS.key") || result.GetString("TLS.key") != "" {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+	if !result.IsSet("TLS.forceClientAuth") || result.GetBool("TLS.forceClientAuth") != false {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+	if !result.IsSet("pools") || result.GetUint("pools") != 1 {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+	if !result.IsSet("considerClientPoolPreference") || result.GetBool("considerClientPoolPreference") != true {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+	if !result.IsSet("internal.nodeExpiryTime") || result.GetUint("internal.nodeExpiryTime") != 30 {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+	if !result.IsSet("internal.nodeExpiryCheckInterval") || result.GetUint("internal.nodeExpiryCheckInterval") != 10 {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+	if !result.IsSet("targetgenerator.bufferSize") || result.GetUint("targetgenerator.bufferSize") != 5 {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+
+	// Pass a viper with a value explicitly set. The value mustn't change.
+	viperWithValue := viper.New()
+	viperWithValue.Set("debug", true)
+	viperWithValue.Set("host", "0.0.0.0")
+	viperWithValue.Set("pools", 5)
+
+	result = utils.ApplyDefaultConfig(viperWithValue)
+	if !result.IsSet("debug") || result.GetBool("debug") != true {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+	if !result.IsSet("listen") {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+	if !result.IsSet("host") || result.GetString("host") != "0.0.0.0" {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+	if !result.IsSet("TLS.enabled") || result.GetBool("TLS.enabled") != false {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+	if !result.IsSet("TLS.CA") || result.GetString("TLS.CA") != "" {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+	if !result.IsSet("TLS.cert") || result.GetString("TLS.cert") != "" {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+	if !result.IsSet("TLS.key") || result.GetString("TLS.key") != "" {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+	if !result.IsSet("TLS.forceClientAuth") || result.GetBool("TLS.forceClientAuth") != false {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+	if !result.IsSet("pools") || result.GetUint("pools") != 5 {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+	if !result.IsSet("considerClientPoolPreference") || result.GetBool("considerClientPoolPreference") != true {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+	if !result.IsSet("internal.nodeExpiryTime") || result.GetUint("internal.nodeExpiryTime") != 30 {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+	if !result.IsSet("internal.nodeExpiryCheckInterval") || result.GetUint("internal.nodeExpiryCheckInterval") != 10 {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+	if !result.IsSet("targetgenerator.bufferSize") || result.GetUint("targetgenerator.bufferSize") != 5 {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+}
+
+func TestApplyDefaultTargetgeneratorStandardConfig(t *testing.T) {
+	var result *viper.Viper
+
+	// Test passing nil to the function
+	result = utils.ApplyDefaultTargetgeneratorStandardConfig(nil)
+	if !result.IsSet("enabled") || result.GetBool("enabled") != false {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+	if !result.IsSet("targets") {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+	if !result.IsSet("targetFile") || result.GetString("targetFile") != "" {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+	if !result.IsSet("tcpports") {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+	if !result.IsSet("udpports") {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+	if !result.IsSet("blacklist") {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+	if !result.IsSet("blacklistFile") || result.GetString("blacklistFile") != "" {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+	if !result.IsSet("maxHostsPerBatch") || result.GetUint("maxHostsPerBatch") != 150 {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+	if !result.IsSet("maxTcpPortsPerBatch") || result.GetUint("maxTcpPortsPerBatch") != 25 {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+	if !result.IsSet("maxUdpPortsPerBatch") || result.GetUint("maxUdpPortsPerBatch") != 25 {
+		t.Errorf("Test failed: Passing nil to config")
+	}
+
+	// Test passing an empty viper to the function
+	emptyViper := viper.New()
+	result = utils.ApplyDefaultTargetgeneratorStandardConfig(emptyViper)
+	if !result.IsSet("enabled") || result.GetBool("enabled") != false {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+	if !result.IsSet("targets") {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+	if !result.IsSet("targetFile") || result.GetString("targetFile") != "" {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+	if !result.IsSet("tcpports") {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+	if !result.IsSet("udpports") {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+	if !result.IsSet("blacklist") {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+	if !result.IsSet("blacklistFile") || result.GetString("blacklistFile") != "" {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+	if !result.IsSet("maxHostsPerBatch") || result.GetUint("maxHostsPerBatch") != 150 {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+	if !result.IsSet("maxTcpPortsPerBatch") || result.GetUint("maxTcpPortsPerBatch") != 25 {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+	if !result.IsSet("maxUdpPortsPerBatch") || result.GetUint("maxUdpPortsPerBatch") != 25 {
+		t.Errorf("Test failed: Passing empty viper to config")
+	}
+
+	// Pass a viper with a value explicitly set. The value mustn't change.
+	viperWithValue := viper.New()
+	viperWithValue.Set("enabled", true)
+	viperWithValue.Set("maxHostsPerBatch", 100)
+	viperWithValue.Set("maxTcpPortsPerBatch", 50)
+	viperWithValue.Set("maxUdpPortsPerBatch", 0)
+	result = utils.ApplyDefaultTargetgeneratorStandardConfig(viperWithValue)
+	if !result.IsSet("enabled") || result.GetBool("enabled") != true {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+	if !result.IsSet("targets") {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+	if !result.IsSet("targetFile") || result.GetString("targetFile") != "" {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+	if !result.IsSet("tcpports") {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+	if !result.IsSet("udpports") {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+	if !result.IsSet("blacklist") {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+	if !result.IsSet("blacklistFile") || result.GetString("blacklistFile") != "" {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+	if !result.IsSet("maxHostsPerBatch") || result.GetUint("maxHostsPerBatch") != 100 {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+	if !result.IsSet("maxTcpPortsPerBatch") || result.GetUint("maxTcpPortsPerBatch") != 50 {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+	if !result.IsSet("maxUdpPortsPerBatch") || result.GetUint("maxUdpPortsPerBatch") != 0 {
+		t.Errorf("Test failed: Passing changed value to config")
+	}
+}
+
+func TestApplyDefaultTargetgeneratorCertificatetransparencyConfig(t *testing.T) {
+	// TODO
+}
+
+func TestApplyDefaultTargetgeneratorLDAPConfig(t *testing.T) {
+	// TODO
+}
+
 func TestApplyDefaultScannerConfig(t *testing.T) {
 	var result *viper.Viper
 
